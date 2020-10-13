8ebbfb167dabd3c05d9025fe3ce0e8e4858f9121
Niklas Vejlby Schjoldager <nikl1565@stud.kea.dk>
2020-10-13 16:49:09 +0200

    Video buttons added

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", init);
 
+// Global
+const htmlBody = document.querySelector("body");
+
 // Game
 const gamePopup = document.querySelector(".js-game-popup");
 const gameChart = document.querySelector(".js-game-chart");
@@ -9,7 +12,7 @@ const gameButton1 = document.querySelector(".js-game-button-1");
 const gameButton2 = document.querySelector(".js-game-button-2");
 const gameButton3 = document.querySelector(".js-game-button-3");
 const gameButton4 = document.querySelector(".js-game-button-4");
-const gameCtaButton = document.querySelector(".js-game-show-popup-button");
+const gameShowPopupButton = document.querySelector(".js-game-show-popup-button");
 const gameStartButton = document.querySelector(".js-game-start-button");
 const gameStopbutton = document.querySelector(".js-game-back-button");
 
@@ -29,25 +32,50 @@ function init() {
 
 
 
-
+// Game
 function initGame() {
     console.log('Initialize game');
 
-    startGame();
+    // Listens for when the user clicks "start quiz" -> startsGame
+    gameShowPopupButton.addEventListener("click", startGame);
 }
 
 function startGame() {
     console.log('Start game');
+
+    gamePopup.classList.remove("is-hidden");
+
+    // Removes the eventlistener, so the user only can click it when the game is closed again.
+    gameShowPopupButton.removeEventListener("click", startGame);
 }
 
 function endGame() {
     console.log('End game');
+
+    // TODO: Reset everything
+
+    gameShowPopupButton.addEventListener("click", startGame);
 }
 
 
 
 
-
+// Video player
 function initVideoPlayer() {
     console.log('Initialize video player');
+
+    videoPlayerVideo.volume = 0.5;
+
+    videoPlayerButton.addEventListener("click", videoPlayerPlayPause);
+}
+
+function videoPlayerPlayPause() {
+
+    if (videoPlayerVideo.paused) {
+        console.log('Play video');
+        videoPlayerVideo.play();
+    } else {
+        console.log('Pause video');
+        videoPlayerVideo.pause();
+    }
 }
